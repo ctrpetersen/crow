6b2ae259ae66a5669c00a4d9f4e7ae0d92166474
Christoffer Petersen <ctrpetersen@outlook.com>
2018-09-18 10:50:31 +0200

    Updated server model

--- Crow/Crow/Model/Server.cs
+++ Crow/Crow/Model/Server.cs
@@ -4,27 +4,43 @@ using Discord.WebSocket;
 
 namespace Crow.Model
 {
+    public enum AnnounceEnum
+    {
+        None,
+        Here,
+        Everyone
+    }
+
     public class Server
     {
         public ulong ServerID { get; set; }
         public char CommandPrefix { get; set; }
-        public List<string> FaqList { get; set; }
+        public Dictionary<string, string> FaqDict { get; set; }
         public ulong ModeratorID { get; set; }
 
         public bool ShouldLog { get; set; }
         public ulong LogChannelID { get; set; }
 
-        public Server(ulong serverID, char commandPrefix, List<string> faqList, ulong moderatorID, bool shouldLog, ulong logChannelID)
+        public bool ShouldTrackTwitch { get; set; }
+        public ulong LiveRoleID { get; set; }
+
+        public bool ShouldAnnounceUpdates { get; set; } //FFFs and game updates
+        public AnnounceEnum AnnounceType { get; set; }
+        public ulong UpdateChannelID { get; set; }
+
+        public bool ShouldAnnounceRedditPosts { get; set; }
+        public ulong RedditFeedChannelID { get; set; }
+
+
+        public Server()
         {
-            ServerID = serverID;
-            CommandPrefix = commandPrefix;
-            FaqList = faqList;
-            ModeratorID = moderatorID;
-            ShouldLog = shouldLog;
-            LogChannelID = logChannelID;
+            ShouldLog = false;
+            ShouldTrackTwitch = false;
+            ShouldAnnounceUpdates = false;
+            ShouldAnnounceRedditPosts = false;
         }
 
-        public string GuildName()
+        public override string ToString()
         {
             return Crow.Instance.Client.GetGuild(ServerID).Name;
         }
