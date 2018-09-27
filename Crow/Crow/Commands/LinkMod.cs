d7ca5a21c43606e0167589a38e52f92cb591aa42
Christoffer Petersen <ctrpetersen@outlook.com>
2018-09-27 22:33:21 +0200

    linkmod and cleaned up unused files

---
+++ Crow/Crow/Commands/LinkMod.cs
@@ -0,0 +1,15 @@
+﻿using Discord.Commands;
+
+namespace Crow.Commands
+{
+    public class LinkMod
+    {
+        [Command("linkmod")]
+        [Summary("Finds a mod from parameter. If the mod name contains spaces, it must be wrapped in quotes.")]
+        [Alias("findmod, mod")]
+        public void LinkModCommand(string name)
+        {
+
+        }
+    }
+}
